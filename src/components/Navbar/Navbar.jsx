acb3a307f76041fbaef91a378f6f3bd8f91c39d1
@aymenmosrati6 <aymenmosrati6@gmail.com>
2024-04-15 17:10:45 +0100

    start of nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+      <div className="py-4">
         <div className="container">
           {/* Logo and Links section */}
           <div>
@@ -13,6 +13,17 @@ const Navbar = () => {
             >
               Eshop
             </a>
+            {/* Menu Items */}
+            <div>
+              <ul>
+                {
+                  MenuLinks.map((data, index)=>(
+
+                  )
+)
+                }
+              </ul>
+            </div>
           </div>
           {/* Navbar Right section  */}
         </div>
