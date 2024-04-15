24fd1fda1759cd985041c91e5978949bd43949c5
@aymenmosrati6 <aymenmosrati6@gmail.com>
2024-04-15 16:56:58 +0100

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <div className="container">
+          {/* Logo and Links section */}
+          <div>
+            <a
+              href="#"
+              className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl"
+            >
+              Eshop
+            </a>
+          </div>
+          {/* Navbar Right section  */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
